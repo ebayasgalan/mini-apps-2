fd5e2e5e39b49f2062f7a6da180499ce81d1bd08
Eric Bayasgalan <ewiz1020@gmail.com>
2021-02-20 11:02:51 -0800

    Initial setup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
 
+app.use(express.json());
+app.use(express.static('./public'));
 
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`server is listening on http://localhost:${PORT}`));
