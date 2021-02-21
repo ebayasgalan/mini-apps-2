ae03fea5e3a083c5cfa9b6098b5429e49ef6841e
Eric Bayasgalan <ewiz1020@gmail.com>
2021-02-20 23:27:09 -0800

    Display bitcoin graph of February

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
 
-const App = () => {
-  return (
-    <div>From App</div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      day: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2021-02-01&end=2021-02-20')
+      .then(result => {
+        let data = [];
+        let day = [];
+        const entry = Object.entries(result.data.bpi);
+        entry.forEach(([key, value], i) => {
+          const val = Math.floor(value);
+          data.push(val);
+          day.push(i);
+        })
+        this.setState({
+          data,
+          day
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <>
+        <Line
+          data={{
+            labels: this.state.day,
+            datasets: [{
+              label: 'February',
+              borderColor: "#3e95cd",
+              data: this.state.data
+            }]
+          }}
+          width={1200}
+          height={300}
+          options={{
+            maintainAspectRatio: false, title: {
+              display: true,
+              text: 'BitCoin Price'
+            }
+          }}
+        />
+      </>
+    )
+  }
 }
 
 export default App;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static('./public'));
 
+// Use a time-series chart and show closing prices only. 
+// By default, this will return the previous 31 days' worth of data. This endpoint accepts the following optional parameters: 
+// 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05';
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`server is listening on http://localhost:${PORT}`));
